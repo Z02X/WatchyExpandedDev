b84fc27b802a7d25f2718f90f62e129632ce5eaa
Michael-Paul Moore <z02x.pr@gmail.com>
2022-04-25 09:10:22 -0700

    Moved many features out into apps.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 // Expanded
 #include <WatchyExpanded.h>
 
+// Faces
+#include "TimeWatchFace.h"
+#include "DateWatchFace.h"
+
+// Apps
+#include "SyncNTP.h"
+#include "WiFiSetup.h"
+
 void setup()
 {
+  //Serial.begin(115200);
   // After each deep sleep this gets run
   CWatchyExpanded watch;
+  watch.AddWatchFace<CTimeWatchFace>();
+  watch.AddWatchFace<CDateWatchFace>();
+  watch.AddApp<CSyncNTP>();
+  watch.AddApp<CWiFiSetup>();
   watch.Run();
 }
 
